fef52a07fd15c922f9440b0accd5c79b7d05d4a1
mohinipatil17408 <mohinipatil17408@gmail.com>
2026-07-30 15:46:34 +0530

    Add basic addition program in Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter first number";
+    cin>>a;
+    cout<<"Enter Second Number";
+    cin>>b;
+    sum = a + b;
+    cout<<"Addition ="<<sum;
+    return 0;
+}
