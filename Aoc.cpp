ebc985aa6731837653c47be2ec735c766c3b0662
mohinipatil17408 <mohinipatil17408@gmail.com>
2026-07-30 15:48:41 +0530

    Add program to calculate area of a circle

---
+++ Aoc.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,area;
+    cout<<"Enter radius of circle";
+    cin>>r;
+    area = r*r*3.14;
+    cout<<"Area ="<<area;
+    return 0;
+}
