2df4bef60e780d1dcd0f542dee863f850d9fd77f
mohinipatil17408 <mohinipatil17408@gmail.com>
2026-07-30 15:49:44 +0530

    Add Even odd.cpp to display odd and even numbers

---
+++ Even odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        cout<<"odd numbers from 1-20 are :\n";
+    for(int i=1;i<=20;i+=2)   
+        cout<<i<<"\t";
+    cout<<"\n";
+        cout<<"even numbers from 1-20 :\n";
+    for(int i=0;i<=20;i+=2)   
+         cout<<i<<"\t";
+    return 0;
+}
