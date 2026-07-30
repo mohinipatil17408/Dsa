a433b5b85521f80f11c8729f279a8ea49ed96e0d
mohinipatil17408 <mohinipatil17408@gmail.com>
2026-07-30 15:47:50 +0530

    Add program to calculate area of a triangle

---
+++ Area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h,area;
+    cout<<"Enter base length of triangle";
+    cin>>b;
+    cout<<"Enter height length of triangle";
+    cin>>h;
+    area = b * h * 0.5;
+    cout<<"Area ="<<area;
+    return 0; 
+}
